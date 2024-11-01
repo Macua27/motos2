6f555f3c6e15b4be1a28ad60dfc559b8fccd8f24
Macua27 <marcos27salazar2008@gmail.com>
2024-11-01 10:57:36 -0600

    Add files via upload

---
+++ alert.js
@@ -0,0 +1,9 @@
+// Pide al usuario su edad
+let edad = prompt("Por favor, ingresa tu edad:");
+
+// Verifica que la entrada no esté vacía y sea un número
+if (edad) {
+    alert("Tienes " + edad + " años.");
+} else {
+    alert("No ingresaste una edad válida.");
+}
